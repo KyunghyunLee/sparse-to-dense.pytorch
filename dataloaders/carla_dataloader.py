664a41416853179b3f5a32902893b6e400e5148b
KyunghyunLee <damul731@gmail.com>
2018-12-17 14:18:15 +0900

    output dimension bug fix

--- dataloaders/carla_dataloader.py
+++ dataloaders/carla_dataloader.py
@@ -54,7 +54,7 @@ class CarlaDataset(MyDataloader):
 
         # perform 1st step of data augmentation
         transform = transforms.Compose([
-            transforms.Resize(200.0 / iheight), # this is for computational efficiency, since rotation can be slow
+            transforms.Resize(150.0 / iheight), # this is for computational efficiency, since rotation can be slow
             # transforms.Rotate(angle),
             # transforms.Resize(s),
             # transforms.CenterCrop(self.output_size),
@@ -79,7 +79,7 @@ class CarlaDataset(MyDataloader):
 
         # perform 1st step of data augmentation
         transform = transforms.Compose([
-            transforms.Resize(200.0 / iheight), # this is for computational efficiency, since rotation can be slow
+            transforms.Resize(150.0 / iheight), # this is for computational efficiency, since rotation can be slow
             # transforms.Rotate(angle),
             # transforms.Resize(s),
             # transforms.CenterCrop(self.output_size),
@@ -95,7 +95,7 @@ class CarlaDataset(MyDataloader):
     def val_transform_label(self, rgb, depth, label):
         depth_np = depth
         transform = transforms.Compose([
-            transforms.Resize(200.0 / iheight),
+            transforms.Resize(150.0 / iheight),
             # transforms.CenterCrop(self.output_size),
         ])
         # label_transform = transforms.CenterCrop(self.output_size),
@@ -109,7 +109,7 @@ class CarlaDataset(MyDataloader):
     def val_transform(self, rgb, depth):
         depth_np = depth
         transform = transforms.Compose([
-            transforms.Resize(200.0 / iheight),
+            transforms.Resize(150.0 / iheight),
             # transforms.CenterCrop(self.output_size),
         ])
         rgb_np = transform(rgb)
